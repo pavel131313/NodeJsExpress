4ac1fd940940080751e7f70560acb6af18c8f15b
pavel131313 <pavelstravkins@gmail.com>
2022-11-17 19:32:28 +0200

    initial commit
    initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'd3169dac8be682d9764e');
---
+++ server/settings/routes.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = (app) => {
+  const passport = require('passport')
+  const usersController = require('./../Controller/UsersController')
+  const postsController = require('./../Controller/PostsController')
+
+
+
+  app
+    .route('/api/auth/signup')
+    .post(usersController.signup)
+
+  app
+    .route('/api/auth/signin')
+    .get(usersController.signin)
+
+  app
+    .route('/api/users')
+    .get(passport.authenticate('jwt', { session: false }), usersController.getAllUsers)
+
+  app
+    .route('/api/posts')
+    .get(passport.authenticate('jwt', { session: false }), postsController.getPosts)
+
+}
