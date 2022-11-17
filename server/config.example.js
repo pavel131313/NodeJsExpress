0b7f12c0f3a3f8910074c77657224674f703d956
pavel131313 <pavelstravkins@gmail.com>
2022-11-17 19:40:48 +0200

    Initial commit #2

---
+++ server/config.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  HOST: 'localhost || 127.0.0.1',
+  PORT: 10005,
+  SOCKET: 'mysql.sock', //If you need socket path
+  DBUSER: 'root',
+  DBPASSWORD: 'root',
+  DBNAME: 'local',
+  jwt: 'your jwt key' //Your jwt key for token generate
+}
