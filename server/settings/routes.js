ace1d928c5982a0be22cb1897ead4f230561be36
pavel131313 <60789594+pavel131313@users.noreply.github.com>
2022-11-18 19:58:55 +0200

    Update routes.js

--- server/settings/routes.js
+++ server/settings/routes.js
@@ -2,19 +2,9 @@
 
 module.exports = (app) => {
   const candidatesController = require('./../Controller/CandidatesController')
-  const usersController = require('./../Controller/UsersController')
-  const postsController = require('./../Controller/PostsController')
 
   app
     .route('/api/candidates')
     .get(candidatesController.getAllCandidates)
-
-  app
-    .route('/api/users')
-    .get(usersController.getAllUsers)
-
-  app
-    .route('/api/posts')
-    .get(postsController.getPosts)
 
 }
